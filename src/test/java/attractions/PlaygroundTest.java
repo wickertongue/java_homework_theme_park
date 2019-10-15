6048ad3680998faec1b4498175ae4944a88d6f75
hayden <haydenwb@gmail.com>
2019-10-15 16:42:06 +0100

    implemented ISec on Playground with tests passing

--- src/test/java/attractions/PlaygroundTest.java
+++ src/test/java/attractions/PlaygroundTest.java
@@ -2,15 +2,20 @@ package attractions;
 
 import org.junit.Before;
 import org.junit.Test;
+import people.Visitor;
 
 import static org.junit.Assert.assertEquals;
 
 public class PlaygroundTest {
     Playground playground;
+    Visitor child;
+    Visitor adult;
 
     @Before
     public void setUp() throws Exception {
         playground = new Playground("Fun Zone", 7);
+        child = new Visitor(10,143.5, 2.50);
+        adult = new Visitor(38, 175.8, 50.23);
     }
 
     @Test
@@ -26,5 +31,15 @@ public class PlaygroundTest {
     @Test
     public void hasVisitCount() {
         assertEquals(0, playground.getVisitCount());
+    }
+
+    @Test
+    public void visitorIsAllowedInThePlayground() {
+        assertEquals(true, playground.isAllowedTo(child));
+    }
+
+    @Test
+    public void visitorIsNotAllowedInThePlayground() {
+        assertEquals(false, playground.isAllowedTo(adult));
     }
 }
