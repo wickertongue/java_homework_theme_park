b2d8594409bdf487683fcf0119655f0d435cf241
hayden <haydenwb@gmail.com>
2019-10-15 16:32:03 +0100

    created all abstract methods for abstract classes

--- src/main/java/behaviours/IReviewed.java
+++ src/main/java/behaviours/IReviewed.java
@@ -1,4 +1,9 @@
 package behaviours;
 
 public interface IReviewed {
+
+    public int getRating();
+
+    public String getName();
+
 }
--- src/main/java/behaviours/ISecurity.java
+++ src/main/java/behaviours/ISecurity.java
@@ -1,5 +1,7 @@
 package behaviours;
 
+import people.Visitor;
+
 public interface ISecurity {
 
     public boolean isAllowedTo(Visitor visitor);
